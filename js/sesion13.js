8cf128ff51b3df1eef1a1a2827483bfd4a0c9750
itamar <itamar@example.com>
2023-10-18 20:35:34 +0200

    Ejercicio de A y B 18/10/23

--- js/sesion13.js
+++ js/sesion13.js
@@ -5,3 +5,25 @@ let array = arxiu.split(".");
 console.log(array[array.length-1]);
 }
 
+function letrasAB(){
+    let texto = prompt("Introduce el texto a mirar las letras A y B");
+    let contA= 0;
+    let contB= 0;
+
+    for (let i = 0 ; i <= texto.length ; i++){
+        if(texto.charAt(i)=== 'a' || texto.charAt(i)=== 'A'){
+            contA++;
+        }
+        if(texto.charAt(i)=== 'b' || texto.charAt(i)=== 'B')
+            contB++;
+    }
+
+    if(contA == contB){
+        console.log("Hay la misma cantidad de A y B");
+    }else{
+        console.log("No hay la misma cantidad de A y B");
+    }
+
+    console.log("Hay " + contA + " letras a en el texto.")
+    console.log("Hay " + contB + " letras b en el texto.")
+}
